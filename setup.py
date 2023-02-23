8c759d247e2a7522de4376f559ac42cd324065e1
“Maxwell <skhokho777@gmail.com>
2023-02-23 08:47:09 +0200

    First Python Package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/Thembezakhe/MyPackage',
+    author='Maxwell',
+    author_email='skhokho777@gmail.com'
+)
